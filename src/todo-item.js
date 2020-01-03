48289a2c355ef98d75f1176a35541729b1c89cac
Gideon-Felt <gideon@feltslanding.com>
2020-01-03 09:59:55 -0700

    toggle done method connected to api

--- src/todo-item.js
+++ src/todo-item.js
@@ -3,14 +3,38 @@ import React, { Component } from "react";
 class TodoItem extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      done: props.item.done
+    }
+
+  }
+
+  toggleDone = () => {
+    fetch(`http://localhost:5000/todo/${this.props.item.id}`, {
+      method:"PATCH",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        done: !this.state.done
+      })
+    })
+    .then(
+      this.setState({
+        done: !this.state.done
+      })
+    )
+    .catch(error => console.log("setState Error: " + error))
   }
 
   render() {
     console.log(this.props.item);
     return (
       <div className="todo-item">
-        <input type="checkbox" />
-        <p>{this.props.item.title}</p>
+        <input type="checkbox"
+        defaultChecked={this.state.done}
+        onClick={this.toggleDone}
+        />
+        <p className={this.state.done ? "done" : null}>{this.props.item.title}</p>
       </div>
     );
   }
